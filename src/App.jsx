bd6e1babf971dfd1c06f752e66e663999d9b79ad
Govind-Asawa <govindasawa23@gmail.com>
2024-05-18 02:50:42 -0500

    Added basic skills page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div className='flex flex-col'>
       <Navbar/>
       <Home />
+      <Skills />
     </div>
   );
 }
