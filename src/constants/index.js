a44c212fcc5b7254267218fa716d6d238de7b1b9
Govind-Asawa <govindasawa23@gmail.com>
2024-05-19 18:11:01 -0500

    Added skills to the about section

---
+++ src/constants/index.js
@@ -0,0 +1,66 @@
+export const SKILLS = [
+    {
+        name: 'Web Dev',
+        percent: '90'
+    },
+    {
+        name: 'machine Learning',
+        percent: '80'
+    },
+    {
+        name: 'Data Analytics',
+        percent: '75'
+    },
+    {
+        name: 'REST API',
+        percent: '85'
+    },
+    {
+        name: 'Databases',
+        percent: '80'
+    },
+    {
+        name: 'java',
+        percent: '90'
+    },
+    {
+        name: 'python',
+        percent: '85'
+    },
+    {
+        name: 'javascript',
+        percent: '80'
+    },
+    {
+        name: 'nodejs',
+        percent: '80'
+    },
+    {
+        name: 'expressjs',
+        percent: '85'
+    },
+    {
+        name: 'reactjs',
+        percent: '75'
+    },
+    {
+        name: 'tailwindCSS',
+        percent: '75'
+    },
+    {
+        name: 'SQL',
+        percent: '80'
+    },
+    {
+        name: 'mySql',
+        percent: '90'
+    },
+    {
+        name: 'mongoDB',
+        percent: '85'
+    },
+    {
+        name: 'AWS(S3,lambda)',
+        percent: '80'
+    },
+]
