42752d4682224d6a67ec3ae329e425f5ccb9be05
Govind-Asawa <govindasawa23@gmail.com>
2024-05-21 17:52:12 -0500

    basic animation to about section. except skills

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,35 @@
 import { IoIosArrowRoundForward } from 'react-icons/io';
 import { FaRegFilePdf } from 'react-icons/fa';
+import { motion } from 'framer-motion';
 import AboutItem from './AboutItem';
 import SkillItem from './SkillItem';
 import aboutImg from '../assets/aboutImg.jpg';
-import {SKILLS} from '../constants'
+import { SKILLS } from '../constants';
 
 export default function About() {
   return (
-    <section className='section-padding bg-black text-white border-b border-neutral-800' id='about'>
+    <section
+      className='section-padding bg-black text-white border-b border-neutral-800'
+      id='about'
+    >
       <div className='flex flex-col px-2 md:px-10'>
         <div className='flex justify-start sm:justify-center items-start'>
           <div className='flex flex-1 justify-start gap-4'>
-            <img
+            <motion.img
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 0.5, delay: 0.5 }}
               src={aboutImg}
               alt='about profile'
               className='hidden md:flex md:w-1/5 md:h-1/5 rounded-md opacity-70'
             />
             <div className='flex flex-col gap-4 flex-1'>
-              <div className='flex flex-col gap-2 border-b border-gray-600 pb-3'>
+              <motion.div
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: 100 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className='flex flex-col gap-2 border-b border-gray-600 pb-3'
+              >
                 <h3 className='text-2xl font-light tracking-tight capitalize'>
                   Govind asawa
                 </h3>
@@ -25,10 +37,15 @@ export default function About() {
                   software engineer
                 </p>
                 <p className='text-sm capitalize text-gray-500'>
-                  MS CS @ University of North Texas '2025 
+                  MS CS @ University of North Texas '2025
                 </p>
-              </div>
-              <div className='w-full grid grid-cols-1 grid-flow-row justify-items-start sm:grid-cols-2 sm:gap-x-8 h-fit p-2'>
+              </motion.div>
+              <motion.div
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: 100 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className='w-full grid grid-cols-1 grid-flow-row justify-items-start sm:grid-cols-2 sm:gap-x-8 h-fit p-2'
+              >
                 <AboutItem tag='First name' value='govind' />
                 <AboutItem tag='last name' value='asawa' />
                 <AboutItem
@@ -43,8 +60,13 @@ export default function About() {
                 />
                 <AboutItem tag='Address' value='Denton, Texas' />
                 <AboutItem tag='Languages' value='English, Hindi, Telugu' />
-              </div>
-              <div className='flex justify-center md:justify-start flex-wrap gap-4 mt-2 font-light'>
+              </motion.div>
+              <motion.div
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: 100 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className='flex justify-center md:justify-start flex-wrap gap-4 mt-2 font-light'
+              >
                 <div className='group p-[0.1rem] bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 rounded-full'>
                   <a
                     href='https://drive.google.com/file/d/1Boxp2r19OiWXlWwG-9mc0RvLz3X0eHH5/view?usp=drive_link'
@@ -59,13 +81,19 @@ export default function About() {
                   <span className='capitalize'>Let's Connect</span>
                   <IoIosArrowRoundForward className='w-6 h-6 group-hover:rotate-90 duration-100' />
                 </button>
-              </div>
+              </motion.div>
             </div>
           </div>
         </div>
         <div className='mt-20 grid gap-2 grid-cols-1 grid-flow-row justify-items-start sm:grid-cols-2 md:grid-cols-3'>
           {SKILLS.map((skill, i) => {
-            return <SkillItem key = {i} skillname={skill.name} percent={skill.percent}/>
+            return (
+              <SkillItem
+                key={i}
+                skillname={skill.name}
+                percent={skill.percent}
+              />
+            );
           })}
         </div>
       </div>
